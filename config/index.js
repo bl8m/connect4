18232038c1cb6c9fb356e7eb78bfd8c307035b75
m_brunazzi <m.brunazzi@bloomdesign.it>
2019-03-20 23:59:43 +0100

    tentativo di fix della build

--- config/index.js
+++ config/index.js
@@ -38,7 +38,7 @@ module.exports = {
 
   build: {
     // Template for index.html
-    //index: path.resolve(__dirname, '../www/index.html'),
+    index: path.resolve(__dirname, '../www/index.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../www'),
